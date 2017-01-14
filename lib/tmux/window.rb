4215986c46c2f4bd49b863f5c3f12174e4b1bede
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-01-14 13:36:36 +0100

    Implement Window.split

--- lib/tmux/window.rb
+++ lib/tmux/window.rb
@@ -17,14 +17,44 @@ module TMux
       end
     end
 
+    def split(options={})
+      @commands << [
+        'split-window',
+        background,
+        orientation(options),
+        size(options),
+        "-t #{target}".strip,
+      ].compact.join(' ')
+
+      if options[:shell_command]
+        @commands << "send-keys -t #{target(1)} '#{options[:shell_command]}' C-m".strip
+      end
+    end
+
     private
 
-    def target
+    def target(pane=0)
       if session
-        "#{session.name}:#{name}"
+        "#{session.name}:#{name}.#{pane}"
       else
-        name
+        "#{name}.#{pane}"
       end
+    end
+
+    def orientation(options)
+      orientation = options[:orientation].to_s
+      return nil if orientation.empty?
+      mnemonic = orientation.chars.first
+      raise "Unknown orientation '#{orientation}'" unless %w(h v).include?(mnemonic)
+      "-#{mnemonic}"
+    end
+
+    def size(options)
+      "-l #{options[:size]}" if options[:size]
+    end
+
+    def background
+      '-d'
     end
   end
 end
--- spec/tmux/window_spec.rb
+++ spec/tmux/window_spec.rb
@@ -13,13 +13,43 @@ RSpec.describe TMux::Window do
   context 'without an owning session' do
     it 'produces a send-keys command when typing a single command' do
       window.type('command_a')
-      expect(window.commands).to include("send-keys -t test-window 'command_a' C-m")
+      expect(window.commands).to include("send-keys -t test-window.0 'command_a' C-m")
     end
 
     it 'produces send-keys commands for all typed commands' do
       window.type('command_a', 'command_b')
-      expect(window.commands).to include("send-keys -t test-window 'command_a' C-m")
-      expect(window.commands).to include("send-keys -t test-window 'command_b' C-m")
+      expect(window.commands).to include("send-keys -t test-window.0 'command_a' C-m")
+      expect(window.commands).to include("send-keys -t test-window.0 'command_b' C-m")
+    end
+
+    it 'can split off a new pane' do
+      window.split
+      expect(window.commands).to include('split-window -d -t test-window.0')
+    end
+
+    it 'can split off a new pane with a specifically horizontal orientation' do
+      window.split(orientation: :horizontal)
+      expect(window.commands).to include('split-window -d -h -t test-window.0')
+    end
+
+    it 'can split off a new pane with a specifically vertical orientation' do
+      window.split(orientation: :vertical)
+      expect(window.commands).to include('split-window -d -v -t test-window.0')
+    end
+
+    it 'does not accept an unknown orientation' do
+      expect { window.split(orientation: :unknown) }.to raise_error /Unknown orientation/
+    end
+
+    it 'can split off a pane with a specific size' do
+      window.split(size: 42)
+      expect(window.commands).to include('split-window -d -l 42 -t test-window.0')
+    end
+
+    it 'can split off a new pane with a command' do
+      window.split(shell_command: 'ls')
+      expect(window.commands).to include('split-window -d -t test-window.0')
+      expect(window.commands).to include("send-keys -t test-window.1 'ls' C-m")
     end
   end
 
@@ -28,12 +58,17 @@ RSpec.describe TMux::Window do
 
     before do
       window.session = session
+      allow(session).to receive(:name).and_return('test-session')
     end
 
     it 'targets the session of the given window' do
-      allow(session).to receive(:name).and_return('test-session')
       window.type('command_b')
-      expect(window.commands).to include("send-keys -t test-session:test-window 'command_b' C-m")
+      expect(window.commands).to include("send-keys -t test-session:test-window.0 'command_b' C-m")
+    end
+
+    it 'splits a new pane in the owning session' do
+      window.split
+      expect(window.commands).to include('split-window -d -t test-session:test-window.0')
     end
   end
 end
