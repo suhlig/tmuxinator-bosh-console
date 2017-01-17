715d3428e476060747a6f7731d5f893f7a68768d
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-01-17 17:27:52 +0100

    Add tests for default template

--- spec/unit/tmuxinator/bosh/template_spec.rb
+++ spec/unit/tmuxinator/bosh/template_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 require 'tmuxinator/bosh/console/template'
+require 'yaml'
 
 RSpec.describe Tmuxinator::BOSH::Console::Template do
   subject(:template) { Tmuxinator::BOSH::Console::Template.new(template_source) }
@@ -20,5 +21,28 @@ RSpec.describe Tmuxinator::BOSH::Console::Template do
     bar = 'baz'
     allow(template_source).to receive(:read).and_return 'foo <%= bar %>'
     expect(template.render(binding)).to eq('foo baz')
+  end
+
+  context 'using the default template' do
+    let(:project_name) { 'test' }
+    let(:instances) { [] }
+    let(:template_source) {
+      (Pathname(__dir__).parent.parent.parent.parent / 'templates/bosh-console.yml')
+    }
+
+    context 'no instances' do
+      let(:result) { template.render(binding) }
+      it 'produces valid YAML' do
+        expect { YAML.load(result) }.to_not raise_error
+      end
+
+      it 'renders the project name' do
+        expect(YAML.load(result)['name']).to eq('test')
+      end
+
+      it 'renders no windows' do
+        expect(YAML.load(result)['windows']).to be_nil
+      end
+    end
   end
 end
